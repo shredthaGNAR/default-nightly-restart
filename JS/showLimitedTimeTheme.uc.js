b31da322865ce15bd8dd2bc3ab451e995ee10408
GLM <wynstaxxx@gmail.com>
2024-04-11 08:17:48 -0700

    add rounded edges

--- JS/showLimitedTimeTheme.uc.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name        显示限时主题
-// @include     about:addons
-// ==/UserScript==
-location.href === 'about:addons' && (() => {
-  for(const theme of BuiltInThemes.builtInThemeMap) {
-    delete theme[1].expiry;
-  }
-})();
